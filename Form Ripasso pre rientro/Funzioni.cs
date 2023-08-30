2cfda146f4faa43af1659d0aaed86251cb322f90
thomasrota <rota.thomas.studente@itispaleocapa.it>
2023-08-30 14:51:13 +0200

    <funzione CheckLunghezzaFissa>

--- Form Ripasso pre rientro/Funzioni.cs
+++ Form Ripasso pre rientro/Funzioni.cs
@@ -9,6 +9,19 @@ namespace Form_Ripasso_pre_rientro
 {
     public class Funzioni
     {
-
+        // Funzione che controlla se il file csv ha la lunghezza fissa
+        public bool CheckLughezzaFissa(string path)
+        {
+            bool check = false;
+            string line;
+            using (StreamReader sr = File.OpenText(path))
+            {
+                line = sr.ReadLine();
+                if (line.Contains("##"))
+                    check = true;
+                sr.Close();
+            }
+            return check;
+        }
     }
 }
